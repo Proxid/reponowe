032512f11a6adad905f36e4532e60717bf9f26b3
MarcinA <68534835+marcin-antczak-naturaily@users.noreply.github.com>
2022-10-13 02:45:23 +0200

    Commit1

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+function zjedz(jedzenie){
+    console.log("Mniam mniam" + jedzenie);
+}
+
+zjedz("banan");
+
+function zjedz1(jedzenie, kupe){
+    console.log("Mniam mniam" + jedzenie + "pyszna" + kupe);
+}
+zjedz1("Dupa","Sraka");
+
+function dodaj(a,b){
+    return a+b ;
+}
+*/
+``
